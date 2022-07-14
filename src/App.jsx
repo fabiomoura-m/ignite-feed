8f42eedf044356fba323c8cd8b99e7b039780c3b
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-07-14 15:52:20 -0300

    Estrutura da aplicação - Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
-    return <Header />;
+    return (
+        <div>
+            <Header />
+
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>posts</main>
+            </div>
+        </div>
+    );
 }
