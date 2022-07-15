62a08972ef6d4ede93175c0df7c589d4132be305
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-07-15 10:28:31 -0300

    Estrutura da aplicação - Componente: Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
 import './global.css';
 import styles from './App.module.css';
@@ -11,7 +12,9 @@ export function App() {
 
             <div className={styles.wrapper}>
                 <Sidebar />
-                <main>posts</main>
+                <main>
+                    <Post />
+                </main>
             </div>
         </div>
     );
