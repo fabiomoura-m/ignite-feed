a5d8c1af481f7764a9b490569bf5a80510822979
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-07-18 10:05:33 -0300

    Estrutura da aplicação - Componente: Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,14 @@
 import { Trash, ThumbsUp } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/fabiomoura-m.png" alt="" />
+            <Avatar
+                hasBorder={false}
+                src="https://github.com/fabiomoura-m.png"
+            />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,10 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img
-                        className={styles.avatar}
-                        src="https://avatars.githubusercontent.com/u/93055468?v=4"
-                    />
+                    <Avatar src="https://github.com/fabiomoura-m.png" />
                     <div className={styles.authorInfo}>
                         <strong>Fabio Moura</strong>
                         <span>Web Developer</span>
